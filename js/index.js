aaecd3be4ec8e843e5fdc83ca9b2e7bb631aa296
Stanislau Vyrski <vyr4376@yandex.ru>
2024-02-01 16:35:23 +0300

    feat: added a blur effect to images in the service block when buttons are pressed (Thu, Feb 1, 2024, 4:34:54 PM)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,110 @@
-"use strict";var body=document.body,header=document.querySelector(".header"),burger=document.querySelector(".burger"),burgerLine=document.querySelectorAll(".burger__line"),navList=document.querySelector(".nav__list");function manageStateBurger(e){body.classList[e]("body_active"),burger.classList[e]("burger_active"),burgerLine.forEach((function(t){return t.classList[e]("burger__line_active")})),navList.classList[e]("nav__list_active")}header.addEventListener("click",(function(e){(e.target.classList.contains("burger")||e.target.classList.contains("burger__line"))&&manageStateBurger("toggle"),"LI"!==e.target.tagName&&"A"!==e.target.tagName||manageStateBurger("remove")}));
+const body = document.body
+const header = document.querySelector('.header')
+const serviceButtonsWrapper = document.querySelector('.service__control-buttons')
+const serviceCards = document.querySelectorAll('.service__cards-figure')
+const serviceButtons = document.querySelectorAll('.service__control-btn')
+const serviceButtonsArray = Array.from(serviceButtons)
+const burger = document.querySelector('.burger');
+const burgerLine = document.querySelectorAll('.burger__line')
+const navList = document.querySelector('.nav__list')
+let activeElements
+let searchDisabledButton
+
+function manageStateBurger(value) {
+	body.classList[value]('body_active')
+	burger.classList[value]('burger_active')
+	burgerLine.forEach(e => e.classList[value]('burger__line_active'))
+	navList.classList[value]('nav__list_active')
+}
+
+header.addEventListener('click', function (e) {
+	if (e.target.classList.contains('burger') || e.target.classList.contains('burger__line')) {
+		manageStateBurger('toggle')
+	}
+	if (e.target.tagName === 'LI' || e.target.tagName === 'A') {
+		manageStateBurger('remove')
+	}
+})
+
+const oneBtnActiveCombo = {
+	'gardeners': [0, 4],
+	'lawn': [2],
+	'planting': [1, 3, 5]
+}
+
+const twoBtnActiveCombo = {
+	'gl': [1, 3, 5],
+	'gp': [2],
+	'lp': [0, 4]
+}
+
+function resetServiceCards() {
+	serviceCards.forEach((e) => {
+		e.classList.remove('service__cards-figure_active')
+	})
+}
+
+function resetDisabledButtons() {
+	serviceButtons.forEach(e => {
+		e.classList.remove('btn_disabled')
+		e.removeAttribute('disabled');
+	})
+}
+
+function checkOneBtnActiveCombination(valueName, loopVar) {
+	return activeElements[0].dataset.value === valueName && !oneBtnActiveCombo[valueName].includes(loopVar)
+}
+
+function checkTwoBtnActiveCombination(firstValue, secondValue, valueName, loopVar) {
+	return activeElements[0].dataset.value === firstValue && activeElements[1].dataset.value === secondValue && twoBtnActiveCombo[valueName].includes(loopVar)
+}
+
+function activeServiceCardsOneBtnActive() {
+	serviceCards.forEach((e, i) => {
+		if (checkOneBtnActiveCombination('gardeners', i)) {
+			e.classList.add('service__cards-figure_active')
+		} else if (checkOneBtnActiveCombination('lawn', i)) {
+			e.classList.add('service__cards-figure_active')
+		} else if (checkOneBtnActiveCombination('planting', i)) {
+			e.classList.add('service__cards-figure_active')
+		}
+	})
+}
+
+function activeServiceCardsTwoBtnActive() {
+	serviceCards.forEach((e, i) => {
+		e.classList.remove('service__cards-figure_active')
+		if (checkTwoBtnActiveCombination('gardeners', 'lawn', 'gl', i)) {
+			e.classList.add('service__cards-figure_active')
+		} else if (checkTwoBtnActiveCombination('gardeners', 'planting', 'gp', i)) {
+			e.classList.add('service__cards-figure_active')
+		} else if (checkTwoBtnActiveCombination('lawn', 'planting', 'lp', i)) {
+			e.classList.add('service__cards-figure_active')
+		}
+	})
+}
+
+function addDisabledButton() {
+	searchDisabledButton = serviceButtonsArray.find(e => !activeElements.includes(e))
+	searchDisabledButton.classList.add('btn_disabled')
+	searchDisabledButton.setAttribute('disabled', true);
+}
+
+serviceButtonsWrapper.addEventListener('click', function (e) {
+	let target = e.target
+
+	target.tagName === 'BUTTON' ? target.classList.toggle('btn_active') : ''
+
+	activeElements = serviceButtonsArray.filter(e => e.classList.contains('btn_active'))
+
+	if (activeElements.length === 0) {
+		resetServiceCards()
+	} else if (activeElements.length === 1) {
+		resetDisabledButtons()
+		activeServiceCardsOneBtnActive()
+	} else if (activeElements.length === 2) {
+		addDisabledButton()
+		activeServiceCardsTwoBtnActive()
+	}
+})
+
