589cd4e67f7f3312ed3fe620ac5c6a7c10e9a8a0
Stanislau Vyrski <vyr4376@yandex.ru>
2024-02-04 19:32:57 +0300

    feat:  performed a dropdown menu in the prices sectio (Sun, Feb 4, 2024, 7:32:48 PM)

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,10 @@ const navList = document.querySelector('.nav__list')
 let activeElements
 let searchDisabledButton
 
+const tariffButtons = document.querySelector('.tariff__buttons')
+const tariffButton = document.querySelectorAll('.tariff__buttons-btn')
+const tariffButtonsSvg = document.querySelectorAll('.tariff__buttons-svg')
+
 function manageStateBurger(value) {
 	body.classList[value]('body_active')
 	burger.classList[value]('burger_active')
@@ -108,3 +112,24 @@ serviceButtonsWrapper.addEventListener('click', function (e) {
 	}
 })
 
+
+const dataSetArr = ['btn-one', 'btn-two', 'btn-three'];
+
+tariffButtons.addEventListener('click', function (event) {
+	const target = event.target
+	const closestDiv = target.closest('div');
+	const closestSvg = Array.from(closestDiv.children).find(e => e.tagName === 'svg')
+	const activeButtons = Array.from(tariffButton).filter(e => e !== closestDiv);
+	const activeIcons = Array.from(tariffButtonsSvg).filter(e => e !== closestSvg);
+
+	function showActiveBlock() {
+		closestDiv.classList.toggle('tariff__buttons-btn_active')
+		closestSvg.classList.toggle('tariff__buttons-svg_active')
+		activeButtons.forEach(e => e.classList.remove('tariff__buttons-btn_active'))
+		activeIcons.forEach(e => e.classList.remove('tariff__buttons-svg_active'))
+	}
+
+	if (dataSetArr.includes(target.dataset.value)) {
+		showActiveBlock()
+	}
+})
