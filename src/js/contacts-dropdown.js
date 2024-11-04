826ef48d6a84bfc86562d889e969c0cc17a3d4b2
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-11-04 17:02:10 +0300

    refactor: refactoring was performed. the code structure was improved. (Mon, Nov 4, 2024, 5:02:03 PM)

--- src/js/contacts-dropdown.js
+++ src/js/contacts-dropdown.js
@@ -1,25 +1,10 @@
-const city = {
-	Canandaigua: {
-		city: "Canandaigua, NY",
-		phone: "+1 585 393 0001",
-		adress: "151 Charlotte Street"
-	},
-	NewYork: {
-		city: "New York City",
-		phone: "+1 212 456 0002",
-		adress: "9 East 91st Street"
-	},
-	Yonkers: {
-		city: "Yonkers, NY",
-		phone: "+1 914 678 0003",
-		adress: "511 Warburton Ave"
-	},
-	Sherill: {
-		city: "Sherrill, NY",
-		phone: "+1 315 908 0004",
-		adress: "14 WEST Noyes BLVD"
-	}
-}
+const city1 = [
+	["Canandaigua, NY", "+1 585 393 0001", "151 Charlotte Street"],
+	["New York City", "+1 212 456 0002", "9 East 91st Street"],
+	["Yonkers, NY", "+1 914 678 0003", "511 Warburton Ave"],
+	["Sherrill, NY", "+1 315 908 0004", "14 WEST Noyes BLVD"]
+]
+
 const contactsDetailsWrapper = document.querySelector(
 	".contacts__details-wrapper"
 )
@@ -27,7 +12,6 @@ const contactsDetails = document.querySelector(".contacts__details")
 const contactDetailsSummary = document.querySelector(
 	".contacts__details-summary"
 )
-const contactDetailsSummaryText = contactDetailsSummary.querySelector("span")
 const popUp = document.querySelector(".contacts__popUp")
 const popUpValues = document.querySelectorAll(".contacts__popUp-value p")
 const popUpButtonLink = document.querySelector(".contacts__popUp-link")
@@ -38,30 +22,37 @@ function choiceValue() {
 	popUp.classList.add("contacts__popUp_active")
 }
 
-contactsDetails.addEventListener("click", function (e) {
-	if (contactsDetails.open === false) {
+const setSummaryText = (value) => (contactDetailsSummary.textContent = value)
+const checkClickOnSummary = (e) => setSummaryText(e.target.textContent)
+
+const actionsContactsDetailsOpen = () => {
+	if (!contactsDetails.open) {
 		popUp.classList.remove("contacts__popUp_active")
-		contactDetailsSummaryText.textContent = "City"
+		contactDetailsSummary.classList.remove("contacts__details-summary_active")
+		setSummaryText("City")
 	}
+}
+
+const setPopupInfo = (e) => {
+	const popUpValuesArray = Array.from(popUpValues)
 	const datasetValue = e.target.dataset.value
-	contactsDetailsWrapper.classList.add("contacts__details-wrapper_active")
-
-	if (e.target.tagName === "SUMMARY") {
-		contactDetailsSummaryText.textContent = e.target.textContent
-	}
-
-	if (Object.keys(city).includes(datasetValue)) {
-		choiceValue()
-
-		for (let i = 0; i < popUpValues.length; i++) {
-			popUpValues[i].textContent = Object.values(city[datasetValue])[i]
-
-			if (Object.values(city[datasetValue])[i].startsWith("+")) {
-				popUpButtonLink.setAttribute(
-					"href",
-					`tel:${Object.values(city[datasetValue])[i]}`
-				)
-			}
+	popUpValuesArray.map((e, i) => {
+		const currentValue = city1[datasetValue][i]
+		e.textContent = currentValue
+		if (currentValue.startsWith("+")) {
+			popUpButtonLink.setAttribute("href", `tel:${currentValue}`)
 		}
+	})
+}
+
+contactsDetails.addEventListener("click", function (e) {
+	contactsDetailsWrapper.classList.add("contacts__details-wrapper_active")
+	actionsContactsDetailsOpen()
+
+	if (e.target.classList.contains("contacts__details-inner-summary")) {
+		setSummaryText(e.target.textContent)
+		choiceValue()
+		checkClickOnSummary(e)
+		setPopupInfo(e)
 	}
 })
