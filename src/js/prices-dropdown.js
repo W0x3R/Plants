490ff2daf3fbbb0d9596a9d341be9930de200c66
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-11-04 21:49:17 +0300

    refactor: improved code readability.removed date attributes. (Mon, Nov 4, 2024, 9:49:09 PM)

--- src/js/prices-dropdown.js
+++ src/js/prices-dropdown.js
@@ -1,23 +1,40 @@
-const tariffButtonsWrapper = document.querySelector('.tariff__buttons')
-const tariffButtons = document.querySelectorAll('.tariff__buttons-btn')
-const tariffButtonsSvg = document.querySelectorAll('.tariff__buttons-svg')
-const dataSetArr = ['btn-one', 'btn-two', 'btn-three'];
+const tariffButtonsWrapper = document.querySelector(".tariff__buttons")
+const tariffButtons = document.querySelectorAll(".tariff__buttons-btn")
+const tariffButtonsSvg = document.querySelectorAll(".tariff__buttons-svg")
 
-tariffButtonsWrapper.addEventListener('click', function (event) {
-	const target = event.target
-	const closestDiv = target.closest('div');
-	const closestSvg = Array.from(closestDiv.children).find(e => e.tagName === 'svg')
-	const activeButtons = Array.from(tariffButtons).filter(e => e !== closestDiv);
-	const activeIcons = Array.from(tariffButtonsSvg).filter(e => e !== closestSvg);
+tariffButtonsWrapper.addEventListener("click", function (e) {
+	const findActiveElement = (selector, className) =>
+		Array.from(selector).find((e) => e.classList.contains(className))
+
+	const activeButton = findActiveElement(
+		tariffButtons,
+		"tariff__buttons-btn_active"
+	)
+	const activeSvg = findActiveElement(
+		tariffButtonsSvg,
+		"tariff__buttons-svg_active"
+	)
+
+	const target = e.target
+	const closestDiv = target.closest("div")
+	const closestSvg = Array.from(closestDiv.children).find(
+		(e) => e.tagName === "svg"
+	)
 
 	function showActiveBlock() {
-		closestDiv.classList.toggle('tariff__buttons-btn_active')
-		closestSvg.classList.toggle('tariff__buttons-svg_active')
-		activeButtons.forEach(e => e.classList.remove('tariff__buttons-btn_active'))
-		activeIcons.forEach(e => e.classList.remove('tariff__buttons-svg_active'))
+		if (activeButton && activeSvg) {
+			activeButton.classList.remove("tariff__buttons-btn_active")
+			activeSvg.classList.remove("tariff__buttons-svg_active")
+		}
+		if (activeButton !== closestDiv && activeSvg !== closestSvg) {
+			closestDiv.classList.add("tariff__buttons-btn_active")
+			closestSvg.classList.add("tariff__buttons-svg_active")
+		}
 	}
-
-	if (dataSetArr.includes(target.dataset.value)) {
+	if (
+		e.target.closest(".tariff__buttons-btn") &&
+		!e.target.classList.contains("tariff__buttons-order")
+	) {
 		showActiveBlock()
 	}
 })
