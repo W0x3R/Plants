0b2d56098d2da66be0587f1948e451ff907062cf
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-11-08 19:51:51 +0300

    feat: added functionality to change the source of images. added images for small resolution. old images renamed. (Fri, Nov 8, 2024, 7:51:45 PM)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import "./style.scss"
 import { callClickEvents } from "./js/eventsHandlers/callClickEvents.js"
 import { clickEvents } from "./js/eventsHandlers/clickEvents.js"
 import { setYearFooter } from "./js/setYearFooter.js"
+import {
+	setsServiceImagesSrc,
+	setsServiceImagesSrcThrottle
+} from "./js/blurCards/setsServiceImagesSrc.js"
 
 window.addEventListener("click", (e) => callClickEvents(e, clickEvents))
 
 setYearFooter()
+setsServiceImagesSrc()
+
+window.addEventListener("resize", setsServiceImagesSrcThrottle)
---
+++ src/js/blurCards/setsServiceImagesSrc.js
@@ -0,0 +1,36 @@
+import imageFirst from "../../images/first-card.png"
+import imageSecond from "../../images/second-card.png"
+import imageThird from "../../images/third-card.png"
+import imageFourth from "../../images/fourth-card.png"
+import imageFifth from "../../images/fifth-card.png"
+import imageSixth from "../../images/sixth-card.png"
+import imageFirstSmall from "../../images/first-card-small.png"
+import imageSecondSmall from "../../images/second-card-small.png"
+import imageThirdSmall from "../../images/third-card-small.png"
+import imageFourthSmall from "../../images/fourth-card-small.png"
+import imageFifthSmall from "../../images/fifth-card-small.png"
+import imageSixthSmall from "../../images/sixth-card-small.png"
+import { debounce } from "../debounce"
+
+const serviceCardsImages = document.querySelectorAll(".service__cards-image")
+const bigImages = [
+	"first-card",
+	"second-card",
+	"third-card",
+	"fourth-card",
+	"fifth-card",
+	"sixth-card"
+]
+
+export const setsServiceImagesSrc = () => {
+	const documentWidth = document.documentElement.clientWidth
+	const getTemplateString = (i) =>
+		documentWidth >= 1399
+			? `./assets/images/pictures/${bigImages[i]}.png`
+			: `./assets/images/pictures/${bigImages[i]}-small.png`
+
+	serviceCardsImages.forEach((card, i) => {
+		card.src = getTemplateString(i)
+	})
+}
+export let setsServiceImagesSrcThrottle = debounce(setsServiceImagesSrc, 500)
